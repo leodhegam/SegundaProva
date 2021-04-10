fbeb2cc8b23a0c8760e23e86d27bf73f8e246ac6
leodhegam <leonardo_dhegam@hotmail.com>
2021-04-10 17:11:38 -0300

    question 7,8

---
+++ app/src/main/java/com/tads/segundaprova/SobreFragment.kt
@@ -0,0 +1,21 @@
+package com.tads.segundaprova
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SobreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_sobre, container, false)
+    }
+
+
+}
